318ff2c2627aa714d1df41983a8798a5d9d2c802
Tremho <steve@ohmert.com>
2021-08-09 10:00:39 -0700

    another full test run

--- src/app-core/ComBinder.ts
+++ src/app-core/ComBinder.ts
@@ -63,7 +63,7 @@ export class ComBinder {
             if(!component.btrack) component.btrack = {}
             component.btrack[prop] = true
             this.model.bind(component, section, prop, (comp:any, prop:string, value:any) => {
-                if(comp.btrack && comp.btrack[prop]) {
+                if(prop.substring(prop.length-5) === '-page' || (comp.btrack && comp.btrack[prop])) {
                     bindFunction(comp, locprop, value, updateAlways)
                 }
             })
