c4807aa7f43bacf4cd1922b98e2be18f67c73883
Tremho <steve@ohmert.com>
2021-10-09 12:04:57 -0700

    Set up for bindComponent2 work III

--- src/app-core/StdComp.ts
+++ src/app-core/StdComp.ts
@@ -9,19 +9,19 @@ export default {
     onBeforeMount(props:any, state:any) {
         // @ts-ignore
         if(this.preStdOnBeforeMount) this.preStdOnBeforeMount(props, state)
-        this.bound = new Object()
+        // this.bound = new Object()
         this.comNormal = new ComNormal(this)
         // @ts-ignore
         this.tagName = this.root.tagName.toLowerCase()
         // @ts-ignore
         this.cm = this.com = newCommon(this) // this.cm and this.com are the same
         // @ts-ignore
-        Object.getOwnPropertyNames(props).forEach(p => {
-            // @ts-ignore
-            this.bound[p] = props[p]
-        })
+        // Object.getOwnPropertyNames(props).forEach(p => {
+        //     // @ts-ignore
+        //     this.bound[p] = props[p]
+        // })
         // @ts-ignore
-        this.bound.text = props.text || this.root.tagName
+        // this.bound.text = props.text || this.root.tagName
         state.action = props.action
         // console.log(this.root.tagName, 'onBeforeMount', props, state, this.bound)
         // @ts-ignore
@@ -38,21 +38,21 @@ export default {
         // @ts-ignore
         this.cm.setCommonProps(div, props, this.tagDefaults)
         this.cm.bindComponent2(props)
-        this.cm.bindComponent()
+        // this.cm.bindComponent()
         // @ts-ignore
         if(this.postStdOnMounted) this.postStdOnMounted(props, state)
         },
     onBeforeUpdate(props:any, state:any) {
         // @ts-ignore
         if(this.preStdOnBeforeUpdate) this.preStdOnBeforeUpdate(props, state)
-        const page = this.cm.getPageComponent()
-        const isReset = page && page.isReset()
-        if(isReset) {
-            Object.getOwnPropertyNames(props).forEach(p => {
-                // @ts-ignore
-                this.bound[p] = props[p]
-            })
-        }
+        // const page = this.cm.getPageComponent()
+        // const isReset = page && page.isReset()
+        // if(isReset) {
+        //     Object.getOwnPropertyNames(props).forEach(p => {
+        //         // @ts-ignore
+        //         this.bound[p] = props[p]
+        //     })
+        // }
         // console.log(this.root.tagName, 'onBeforeUpdate', props, state, this.bound)
         // @ts-ignore
         if(this.postStdOnBeforeUpdate) this.postStdOnBeforeUpdate(props, state)
@@ -68,7 +68,7 @@ export default {
         // @ts-ignore
         if(this.preStdOnBeforeUnmount) this.preStdOnBeforeUnmount(props, state)
         // console.log(this.root.tagName, 'onBeforeUnmount', props, state, this.bound)
-        this.bound = {}
+        // this.bound = {}
         // @ts-ignore
         if(this.postStdOnBeforeUnmount) this.postStdOnBeforeUnmount(props, state)
     },
