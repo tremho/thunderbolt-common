ea7d3708804a9196e192fb9a81514fdf154eb98b
Tremho <steve@ohmert.com>
2021-10-20 13:42:31 -0700

    build weirdness

--- src/test-actions/TestOps.ts
+++ src/test-actions/TestOps.ts
@@ -6,7 +6,7 @@ export function initModule(appIn:AppCore) {
     app = appIn
 }
 export function readModelValue(modelPath:string) {
-    console.log('readModelValue @', modelPath)
+    console.log('>readModelValue @', modelPath)
     const resp =  app.model.getAtPath(modelPath)
     console.log('returning ', resp)
     return resp
