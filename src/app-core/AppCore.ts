85044735b39f0f9c42bf8e2158bf4f275805d83f
Tremho <steve@ohmert.com>
2022-01-03 13:18:24 -0800

    askAHuman

--- src/app-core/AppCore.ts
+++ src/app-core/AppCore.ts
@@ -1000,7 +1000,7 @@ export class AppCore {
     connectTestMethods() {
         const callTestRequest = (request:string, params:string[]) => {
             request = request.trim()
-            // console.log('callTestRequest', request, params)
+            console.log('callTestRequest', request, params)
             const ops:any = testOps
             const fn = ops[request]
             const resp =  fn && fn.apply(this, params)
