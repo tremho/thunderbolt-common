17aef100c3e8419c92c9bcd7dcdcf64cebd196e0
Tremho <steve@ohmert.com>
2022-03-24 18:43:13 -0700

    another attempt at the end pan fix

--- src/app-core/ComNormal.ts
+++ src/app-core/ComNormal.ts
@@ -826,8 +826,8 @@ function mobilePanHandler(ev:any) {
     let clientX = touchX
     let clientY = touchY
     if(type === 'end') {
-        clientX = touchX-mx;  // final position on end
-        clientY = touchY-my;
+        clientX = touchX+mx;  // final position on end
+        clientY = touchY+my; // not sure why we hace + here but - below,  but hey.
     }
     // we now include the delta after all
     ed.value = (type === 'change') ? {type, mx:touchX-mx, my:touchY-my, dx:mx, dy:my} : {type, clientX, clientY}
